ca1a4f3d88557ca2c8480cc3a6f88737593b78b9
Duke Greene <Duke@Dukes-MacBook-Pro.local>
2015-10-28 21:51:58 -0500

    comment out reference to gitignored development file

--- config/initializers/app_config.rb
+++ config/initializers/app_config.rb
@@ -1,4 +1,7 @@
 require 'yaml'
+# For use in development only. 'app.yml' is stored locally and gitignored for security. 
+# If this code is not commented out, deploying to production will fail 
+# because 'app.yml' does not exist in the production repository.
 
-yaml_data = YAML::load(ERB.new(IO.read(File.join(Rails.root, 'config', 'app.yml'))).result)
-APP_CONFIG = HashWithIndifferentAccess.new(yaml_data)
+# yaml_data = YAML::load(ERB.new(IO.read(File.join(Rails.root, 'config', 'app.yml'))).result)
+# APP_CONFIG = HashWithIndifferentAccess.new(yaml_data)
