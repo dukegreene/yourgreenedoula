998768b822bafecb19e7a430413d0c6761e41614
Duke Greene <Duke@Dukes-MacBook-Pro.local>
2015-10-28 23:02:45 -0500

    make ActionMailer call explicit in smtp initializer

--- config/initializers/mailer_setup.rb
+++ config/initializers/mailer_setup.rb
@@ -1,10 +1,10 @@
 
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'gmail.com',
-    user_name:            'yourgreenedoula@gmail.com',
-    password:             ENV['GMAIL_PW'],
-    authentication:       'plain',
-    enable_starttls_auto: true 
+  ActionMailer::Base.smtp_settings = {
+    :address =>              'smtp.gmail.com',
+    :port =>                 587,
+    :domain =>               'gmail.com',
+    :user_name =>            'yourgreenedoula@gmail.com',
+    :password =>             ENV['GMAIL_PW'],
+    :authentication =>       'plain',
+    :enable_starttls_auto => true 
   }
